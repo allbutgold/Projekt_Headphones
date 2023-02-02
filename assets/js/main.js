cddcc8080decaa51d30c0ffc272a3a9c76b85107
allbutgold <contact@leokieffer.com>
2023-02-02 19:11:35 +0100

    too long ago

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,5 @@ const darkMode = document.querySelector("#darkMode");
 
 darkMode.addEventListener("click", function() { // on click of button darkMode toggle CSS class .darkMode
     document.body.classList.toggle("darkMode");
+    document.body.classList.toggle("bodyAbout");
 })
